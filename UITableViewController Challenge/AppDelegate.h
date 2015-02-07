e4b070c6ae711cb0f5c5b048da6e87354a04ca9e
pieterkranenburg <pkranenbu@gmail.com>
2015-02-06 22:11:47 -0800

    Everything is Awesome
    Everything is awesome

---
+++ UITableViewController Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge
+//
+//  Created by Pieter Kranenburg on 1/28/15.
+//  Copyright (c) 2015 SocialDyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge
+//
+//  Created by Pieter Kranenburg on 1/28/15.
+//  Copyright (c) 2015 SocialDyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
